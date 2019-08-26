3355420c5137f24bef3afcea8305c3e987eba858
angganova2019 <angga24.opa@gmail.com>
2019-08-26 16:12:21 +0700

    commit senin 26082019

--- src/container/pages/BlogPost/DetailPost/DetailPost.jsx
+++ src/container/pages/BlogPost/DetailPost/DetailPost.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import './DetailPost.css';
 
 class DetailPost extends Component {
+
+    state = {
+        title: '',
+        body: ''
+    }
+
+    componentDidMount() {
+        let id = this.props.match.params.postID;
+        // console.log(this.props);
+        Axios.get(`http://localhost:3004/posts/${id}`).then(res => {
+            this.setState({
+                title: res.data.title,
+                body: res.data.body
+            })
+        })
+    }
     render() {
         return (
-            <p>Detail Post</p>
+            <div className="p-detail-post">
+                <p className="detail-title">{this.state.title}</p>
+                <p className="detail-body">{this.state.body}</p>
+            </div>
         )
     }
 }
